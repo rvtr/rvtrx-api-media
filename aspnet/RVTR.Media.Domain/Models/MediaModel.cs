4467c61a5fdd0b3973cbafbe3ed754bf0ac99541
Darren Yakes <dayakes@gmail.com>
2021-02-19 10:10:34 -0500

    added spacing to model

--- aspnet/RVTR.Media.Domain/Models/MediaModel.cs
+++ aspnet/RVTR.Media.Domain/Models/MediaModel.cs
@@ -44,7 +44,6 @@ namespace RVTR.Media.Domain.Models
       {
         yield return new ValidationResult("URL can not be null.");
       }
-
       if (string.IsNullOrEmpty(AltText))
       {
         yield return new ValidationResult("AltText can not be null.");
