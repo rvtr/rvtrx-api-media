5fbcbe377d0066496e98431f92db6ada3a751b03
Darren Yakes <dayakes@gmail.com>
2021-02-24 11:38:34 -0500

    set EntityId to Now.Ticks

--- aspnet/RVTR.Media.Domain/Abstracts/AEntity.cs
+++ aspnet/RVTR.Media.Domain/Abstracts/AEntity.cs
@@ -6,5 +6,10 @@ namespace RVTR.Media.Domain.Abstracts
   public abstract class AEntity
   {
     public long EntityId { get; set; }
+
+    public AEntity()
+    {
+      EntityId = System.DateTime.Now.Ticks;
+    }
   }
 }
