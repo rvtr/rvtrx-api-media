57bc066a1117f9d511bbc6b8dab155dccf25a3e5
Gledinburgh <gledinburgh@gmail.com>
2021-06-04 16:08:57 -0500

    added deleteAsync test to repository test

--- aspnet/RVTR.Media.Testing/Tests/RepositoryTest.cs
+++ aspnet/RVTR.Media.Testing/Tests/RepositoryTest.cs
@@ -8,7 +8,14 @@ namespace RVTR.Media.Testing.Tests
 {
   public class RepositoryTest : DataTest
   {
-    private readonly MediaModel _media = new MediaModel() { EntityId = 3 };
+    private readonly MediaModel _media = new MediaModel()
+    {
+      EntityId = 3,
+      Group = "string",
+      GroupIdentifier = "string",
+      Uri = "string",
+      AltText = "string"
+    };
 
     [Fact]
     public async void Test_Repository_InsertAsync()
@@ -18,6 +25,22 @@ namespace RVTR.Media.Testing.Tests
         var medias = new Repository<MediaModel>(ctx);
         await medias.InsertAsync(_media);
         Assert.Equal(EntityState.Added, ctx.Entry(_media).State);
+      }
+    }
+
+    ///<summary>
+    /// checks that DeleteAsync function removes entity from context
+    ///</summary>
+    [Fact]
+    public async void Test_Repository_DeleteAsync()
+    {
+      using (var ctx = new MediaContext(Options))
+      {
+        var medias = new Repository<MediaModel>(ctx);
+        ctx.Medias.Add(_media);
+        ctx.SaveChanges();
+        await medias.DeleteAsync(_media.id);
+        Assert.Equal(EntityState.Deleted, ctx.Entry(_media).State);
       }
     }
   }
